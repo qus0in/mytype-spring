ba4f4d16e6771dcbc858b43f7532348f1e635dd1
qus0in <qus0in@gmail.com>
2025-03-19 16:56:27 +0900

    잘가 Service

--- src/main/java/org/example/concertbooking/controller/IndexController.java
+++ src/main/java/org/example/concertbooking/controller/IndexController.java
@@ -1,13 +1,30 @@
 package org.example.concertbooking.controller;
 
+import org.example.concertbooking.model.dto.UserDTO;
+import org.example.concertbooking.model.repository.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+    final private UserRepository userRepository;
+
+    public IndexController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @RequestMapping("/")
     String index() {
         return "index";
+    }
+
+    @PostMapping("/user")
+    String addUser(@RequestBody UserDTO userDTO) throws Exception {
+        userRepository.save(userDTO);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/org/example/concertbooking/model/repository/UserRepository.java
@@ -0,0 +1,27 @@
+package org.example.concertbooking.model.repository;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.example.concertbooking.model.dto.UserDTO;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserRepository implements SupabaseRepository<UserDTO> {
+    final private String tableName = "USER";
+
+    public void save(UserDTO userDTO) throws Exception {
+        save(userDTO, tableName);
+    }
+
+    public List<UserDTO> findAll() throws Exception {
+        String responseJson = findAll(tableName);
+        return objectMapper.readValue(responseJson, new TypeReference<>() {});
+    }
+
+    public UserDTO findById(String userId) throws Exception {
+        String responseJson = findById(userId,
+                tableName, "user_id");
+        return objectMapper.readValue(responseJson, new TypeReference<>() {});
+    }
+}
